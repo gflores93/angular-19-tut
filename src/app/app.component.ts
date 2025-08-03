ab7679d299858e000e2d2622f16cb75d5727bf78
Guillermo Flores <gflores.93@hotmail.com>
2025-08-03 16:10:44 -0600

    feat: example of get and set input field

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,20 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+
+  username: string = "";
+
+  getUserName(event: Event) {
+    this.username = (event.target as HTMLInputElement).value;
+  }
+
+  setUserName() {
+    this.username = 'Peter';
+  }
+
+  getUserNameWithTemplate(name: string) {
+    this.username = name;
+    console.log(name);
+  }
+}
